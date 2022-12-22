558a4decd49d1bb202349122c7a8cadf00b4297b
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2022-12-23 00:44:09 +0300

    получение переменных

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+console.log(todoControl);
+console.log(headerInput);
+console.log(todoList);
+console.log(todoCompleted);

53220158afc61c0e65b7a5c17cf8a578a5716d47
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2022-12-23 01:15:34 +0300

    чистый код после видео

--- js/script.js
+++ js/script.js
@@ -5,7 +5,46 @@ const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
 
-console.log(todoControl);
-console.log(headerInput);
-console.log(todoList);
-console.log(todoCompleted);
+const toDoData = [];
+
+const render = function () {
+    todoList.innerHTML = '';
+    todoCompleted.innerHTML = '';
+    toDoData.forEach(function (item) {
+        // toDoItem
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+
+        li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
+            '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>';
+
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+
+        li.querySelector('.todo-complete').addEventListener('click', function () {
+            item.completed = !item.completed;
+            render();
+        });
+
+    });
+};
+
+todoControl.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const newToDo = {
+        text: headerInput.value,
+        completed: false,
+    };
+
+    toDoData.push(newToDo);
+    headerInput.value = '';
+
+    render();
+}); 
